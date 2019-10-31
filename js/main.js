8747c7a6a4bdb4c45e20574a643ccffa701a605a
Boss0049 <B-Bossza@hotmail.co.th>
2019-10-31 10:26:51 +0700

    Landing Page Resume

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('#ckLine').ckLine({
+    leftRight: false,
+    strokeColor: 'rgba(0,0,0,1)',
+    interval: 800,
+    animateTime: 1600,
+    animationTimeRange: [800, 1600],
+    easing: "swing"
+});
+
+
+
